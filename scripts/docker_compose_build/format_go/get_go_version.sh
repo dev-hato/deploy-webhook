a7d09e5bbaae66b3e9ccc915589af4978348b893
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2022-11-20 12:06:16 +0900

    CIのスクリプトをまとめる (#321)

--- scripts/docker_compose_build/format_go/get_go_version.sh
+++ scripts/docker_compose_build/format_go/get_go_version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker compose -f docker-compose.yml -f dev.docker-compose.yml pull
+
 CMD="go version | awk '{print \$3}' | sed -e 's/^go//g'"
 go_version=$(docker compose -f docker-compose.yml -f dev.docker-compose.yml run deploy-webhook sh -c "${CMD}")
 echo "Go version:" "${go_version}"
--- scripts/docker_compose_build/format_go/install_goimports.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(go env GOPATH)/bin" >>"${GITHUB_PATH}"
-go install golang.org/x/tools/cmd/goimports
