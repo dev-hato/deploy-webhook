db7312c1ddb34052e3d93bea89c42dca1ee616b9
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-20 11:36:44 +0900

    CIのスクリプトをまとめる

--- scripts/docker_compose_build/dockle/run_dockle.sh
+++ scripts/docker_compose_build/dockle/run_dockle.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+dockle_version="$(cat .dockle-version)"
+curl -L -o dockle.deb "https://github.com/goodwithtech/dockle/releases/download/v${dockle_version}/dockle_${dockle_version}_Linux-64bit.deb"
+sudo dpkg -i dockle.deb
+
+docker compose -f docker-compose.yml -f "${DOCKER_COMPOSE_FILE_NAME}" pull
+docker compose -f docker-compose.yml -f "${DOCKER_COMPOSE_FILE_NAME}" up -d
+
 for image_name in $(docker compose -f docker-compose.yml -f "${DOCKER_COMPOSE_FILE_NAME}" images | awk 'OFS=":" {print $2,$3}' | tail -n +2); do
   cmd="dockle --exit-code 1 "
   cmd+="${image_name}"
--- scripts/docker_compose_build/format_go/format_files.sh
+++ scripts/docker_compose_build/format_go/format_files.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "$(go env GOPATH)/bin" >>"${GITHUB_PATH}"
+go install golang.org/x/tools/cmd/goimports
+
 go_version="$(go version | awk '{print $3}' | awk -F . 'BEGIN {OFS="."} {print $1,$2}' | sed -e 's/go//g')"
 go mod tidy -go="${go_version}"
 goimports -l -w .
